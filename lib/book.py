e435817195023a1012ef23ee567a1503df7fd00a
abdalla-wasaa <abdallawasaa7@gmail.com>
2024-01-27 10:31:16 +0300

    Attempted

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count, publication_year):
+        self.title = title
+        self.page_count = page_count  # Add page_count attribute
+        self.publication_year = publication_year
+        self.checked_out = False
+
+    def checkout(self):
+        if not self.checked_out:
+            self.checked_out = True
+            return True
+        else:
+            return False
+
+    def checkin(self):
+        if self.checked_out:
+            self.checked_out = False
+            return True
+        else:
+            return False
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, color):
+        self.brand = brand
+        self.size = size
+        self.color = color
+        self.worn = False
+
+    def wear(self):
+        if not self.worn:
+            self.worn = True
+            return True
+        else:
+            return False
+
+    def clean(self):
+        if self.worn:
+            self.worn = False
+            return True
+        else:
+            return False
